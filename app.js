00efa61085960e35785cd9ea5fef1d5561aee4bd
zang3tsu88 <maximenya.a@gmail.com>
2023-07-09 00:01:27 +0300

    feat: add logger middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -10,9 +11,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use(requestLogger);
 app.use(router);
-app.use(errorHandler);
+app.use(errorLogger);
+
 app.use(errors());
+app.use(errorHandler);
 
 mongoose.connect('mongodb://127.0.0.1:27017/filmsdb')
   .then(() => console.log('Connected to Films DB!'))
