b88a26f69d0e84002c9254a2286067783fe20d21
zang3tsu88 <maximenya.a@gmail.com>
2023-08-16 18:15:41 +0300

    refactor: specify path using 'path' module

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,16 +1,17 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
+const path = require('path');
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'logs/request.log' }),
+    new winston.transports.File({ filename: path.join('logs', 'request.log') }),
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'logs/error.log' }),
+    new winston.transports.File({ filename: path.join('logs', 'error.log') }),
   ],
   format: winston.format.json(),
 });
